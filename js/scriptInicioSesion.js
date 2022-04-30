b830b5da9583e314f8da8a710396e2a40e997575
maroj0 <maroj074@gmail.com>
2022-04-30 18:43:59 -0300

    Funciona

--- js/scriptInicioSesion.js
+++ js/scriptInicioSesion.js
@@ -6,7 +6,17 @@ function Usuario(email, contrasenia){
 
 function mostrar_Mensaje(email, contrasenia){
     let usuario = new Usuario(email,contrasenia);
-    usuario.alertaMensaje
+    $.ajax({
+        url:'prueba.php',
+        method:'POST',
+        data:{
+            user:JSON.stringify(usuario)
+        },
+    }).done((resp)=>{
+        alert("Respuesta de servidor: " + resp);
+    }).fail((err)=>{
+        console.error(" Ocurrio un error " + err)
+    })
 }
 
 
--- prueba.php
+++ prueba.php
@@ -1,4 +1,7 @@
 <?php
-    $usuario = json_decode($_POST['datos']);
-    var_dump($usuario);
+    $entrada = $_POST['user'];
+    $usuario = json_decode($entrada);
+    //echo $entrada;
+    echo 'mapeo de usuario del servidor'.json_encode($usuario);
+    http_response_code(200);
 ?>
