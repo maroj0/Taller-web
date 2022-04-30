42ad256fec43dbda80013210852ec1f11f7c2e92
tomas cardozo <tomicar200894@gmail.com>
2022-04-30 10:49:54 -0300

    Solo falta 2A

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@ const search = new GeoSearch.GeoSearchControl({
     provider: new GeoSearch.OpenStreetMapProvider(),
   });
 
-  const start = async function(consulta = null) {
+  const buscar = async function(){
     var result = null;
     const provider = new GeoSearch.OpenStreetMapProvider();
     var search = document.getElementById('calle').value;
@@ -24,36 +24,22 @@ const search = new GeoSearch.GeoSearchControl({
     search += ', ' + document.getElementById('provincia').value;
     search += ', ' + document.getElementById('pais').value;
 
-    if(consulta){
-        result = await provider.search({ query: consulta });
-     }else{
-        result = await provider.search({ query: search });
-        console.log(result);
-     }
-     if(result != null && result.length > 0){      
-       var zoom = 0;
-       switch(result[0].raw.type.toString()){
-         case 'administrative':
-           zoom = 12;
-           break;
-         case 'residential':
-           zoom= 17;
-           break;
-         default:
-           console.log(result[0].raw.type.toString());
-           zoom= 16;
-       }
-       if(this.marker){
-         this.marker.remove();
-       }      
-       this.map.setView([result[0].y,result[0].x], zoom);     
-       this.marker = L.marker([result[0].y,result[0].x],{title:result[0].label}).addTo(map);      
-       document.getElementById('lat').value = result[0].y;
-       document.getElementById('long').value = result[0].x;
+    result = await provider.search({ query: search });
+    console.log(result);
+
+    if(result != null && result.length > 0){      
+      if(this.marker){
+        this.marker.remove();
+      }
+      var zoom = 17;
+      this.map.setView([result[0].y,result[0].x], zoom);     
+      this.marker = L.marker([result[0].y,result[0].x],{title:result[0].label}).addTo(map);      
+      document.getElementById('lat').value = result[0].y;
+      document.getElementById('long').value = result[0].x;
     }
 }
 
 function buscarUbicacion(){
-    start();
+    buscar();
 }
 
