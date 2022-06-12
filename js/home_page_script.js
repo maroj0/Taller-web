ed83966b34b09b2db1e000ce4f43fbfb9b946c8a
tomas cardozo <tomicar200894@gmail.com>
2022-06-11 23:58:25 -0300

    11/6

--- js/home_page_script.js
+++ js/home_page_script.js
@@ -1,11 +1,7 @@
-$(document).ready(function () {
-    $('#example').DataTable();
-});
-
 function start() {
     // Initializes the client with the API key and the Translate API.
     gapi.client.init({
-      'apiKey': 'AIzaSyAsGZso29QPAEVLl6WRXo_uFn_OVmDg2q0',
+      'apiKey': 'AIzaSyAB8xfQcZeTRIY30eQGPZt61jCsppkhvxc',
       'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
     }).then(function() {
       // Executes an API request, and returns a Promise.
@@ -25,30 +21,20 @@ function start() {
   gapi.load('client', start);
   
   
-  
 // Search for a specified string.
 function search() {
   var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
     q: q,
     part: 'snippet',
-    maxResults: 40
+    maxResults: 10
   });
   request.execute(function(response) {
     console.log(response)
     var playlistItems = response.result.items;
     if (playlistItems) {
-      contador = 0;
-      $('#example').append('<tr>');
       $.each(playlistItems, function(index, item) {
-        if(contador==4){
-          $('#example').append('</tr>');
-          $('#example').append('<tr>');
-          contador=0
-        }
         displayResult(item);
-        contador=contador+1;
-        agregar_video_categoria(item.id.videoId)
       });
     } else {
       $('#example').html('Sorry you have no uploaded videos');
@@ -61,26 +47,38 @@ function agregar_categoria(){
   if(($('#' + titulo_categoria).val())==""){
     alert('La categoria ya existe');
   }else{
-    $('#seccion_resultados').append('<h2>'+ titulo_categoria + '</h2>');
-    $('#seccion_resultados').append('<table id=' + titulo_categoria + ' class="display" style="width:100%">'); 
+    $('#seccion_resultados').append('<div id=' + titulo_categoria + ' class="categoria" style="width:100%"></div>'); 
   }
 }
 
-function displayResult(item) {
+function displayResult(item){
     var videoId = item.id.videoId;
     var linkVideo = 'https://www.youtube.com//embed/'+ videoId
-    $('#example').append('<iframe src="' + linkVideo + '"></iframe>');
+    var cuadro_video = ''
+    cuadro_video = cuadro_video + '<div class="cuadro_video">' + '<iframe src="' + linkVideo + '"></iframe>' + '<button id="' + videoId + '">Agregar Video</button></div>'
+    $('#example').append(cuadro_video);
   }
-  
+
 function agregar_video_categoria(idVideo){
-  if (playlistItems) {
-    $.each(playlistItems, function(index, item) {
-      if(item.id.videoId == idVideo){
-        var linkVideoCategoria = 'https://www.youtube.com//embed/'+ idVideo
-        $('#' + $('#titulo_categoria').val()).append('<iframe src="' + linkVideoCategoria + '"></iframe>');
-      }
-    });
-  } else {
-    $('#example').html('Sorry you have no uploaded videos');
-  }
+  var linkVideoCategoria = 'https://www.youtube.com//embed/'+ idVideo
+  $('#' + $('#titulo_categoria').val()).append('<iframe src="' + linkVideoCategoria + '"></iframe>');
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
